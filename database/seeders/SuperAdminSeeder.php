62ce316a86d5ab287da89205e6ea097dedcb1fa1
killias35 <killias356@gmail.com>
2025-12-18 02:04:22 +0100

    ajout role dans user

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'killias356@gmail.com',
+            'role' => 'super_admin',
+            'pseudo' => 'Killias',
+            'password' => Hash::make('kblebg'),
+        ]);
+    }
+}
